f8281a6e5393f1b734d0e873988939fea46559ca
Gustavo <gusstavocta@hotmail.com>
2024-02-13 16:18:47 -0500

    Validaciones

---
+++ Shared/Constantes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestauranteApp.Shared
+{
+    public class Constantes
+    {
+        public const string CamposRequerido = "El campo {0} es requerido.";
+        public const string ValidacionRango = "El campo {0} debe estar entre {1} y {2}.";
+    }
+}
--- Shared/Request/ProductoDtoRequest.cs
+++ Shared/Request/ProductoDtoRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,21 @@ namespace RestauranteApp.Shared.Request
     public class ProductoDtoRequest
     {
         public int Id { get; set; }
+        [Required(ErrorMessage =Constantes.CamposRequerido)]
+        [MaxLength(100)]
         public string Nombre { get; set; } = default!;
+
+        [Range(1, 9999, ErrorMessage =Constantes.ValidacionRango)]
+        [Display(Name ="Precio Unitario")]
         public float PrecioVenta { get; set; }
+
+        //[Range(1, 9999)]
         public float PrecioCompra { get; set; }
+        //[Range(1, 9999)]
         public int Stock { get; set; }
         public DateTime? FechaVencimiento { get; set; }
+        
+        //[MaxLength(200)]
         public string? Observacion { get; set; }
 
         public int CategoriaId { get; set; }
